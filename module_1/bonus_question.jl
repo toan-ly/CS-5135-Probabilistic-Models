9d08d707543501e170796f1ff6acb9af9535388d
toan-ly <toanly0521@gmail.com>
2025-08-31 18:48:03 -0400

    feat: add exercise 3

---
+++ module_1/bonus_question.jl
@@ -0,0 +1,14 @@
+n = 7
+k = 4
+result = 0.0
+for x1 in 0:n
+    for x2 in 0:(n-x1)
+        for x3 in 0:(n-x1-x2)
+            x4 = n - x1 - x2 - x3
+            global result += (factorial(n) / (factorial(x1) * factorial(x2) * factorial(x3) * factorial(x4)))^2
+            end
+        end
+end
+result /= 4.0^14
+println("Probability that the chef matches the correct order: $result")
+
